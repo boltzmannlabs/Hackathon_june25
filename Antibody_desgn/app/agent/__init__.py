23f4aac54399cb274a0afb57687f73794cf64238
Jayasurya-47 <jayasuryabaipu47@gmail.com>
2025-06-25 12:18:39 +0000

    updated_v1

--- Antibody_desgn/app/agent/__init__.py
+++
@@ -1,7 +0,0 @@
-# from app.tools.preprocessing import run_preprocessing
-# TOOL_FUNCTIONS = {
-#     "preprocessing": run_preprocessing,
-#     "rf_antibody": run_rf_antibody,
-#     "alphafold": run_alphafold_multimer,
-#     "prodigy": run_prodigy,
-# }
